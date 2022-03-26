5d297df3edf64a3af9fba7ae0dd4bbd6008e17cb
Bruna <bruna.chiochetta@gmail.com>
2022-03-26 18:10:50 -0300

    Initialize spring project

---
+++ src/test/java/com/g2/ufrgs/liberumforum/LiberumForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g2.ufrgs.liberumforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiberumForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
